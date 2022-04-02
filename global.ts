66dd69e224871f6ba7c4394088c584ecc817c46f
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2022-04-02 11:25:05 -0500

    ajustes login.ts

---
+++ global.ts
@@ -0,0 +1,13 @@
+'use strict';
+// ==============================Backend==============================
+// ****Pruebas****
+// export const url = "http://localhost:8080";
+
+// ****Produccion****
+export const url = "https://ahp-seminario.herokuapp.com";
+// ==============================Frontend==============================
+// ****Pruebas****
+// export const url_front = "http://localhost:4200";
+
+// ****Produccion****
+export const url_front = "https://ahp-seminario.herokuapp.com";
